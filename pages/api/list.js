f9d492a604873b813adf23054153909bd06d77fa
alexandraarhire <alexandra.arhire1403@gmail.com>
2024-05-07 17:23:43 +0300

    mongo

---
+++ pages/api/list.js
@@ -0,0 +1,25 @@
+import { connectToDatabase } from "../ ../lib/connectToDatabase";
+
+export default async function handler(request, response){
+    try {
+
+   
+        const {mongoClient} = await connectToDatabase();
+        const db = mongoClient.db("sample_restaurants");
+        const collection = db.collection("restaurants");
+        const results = await collection
+        .find({})
+        .project({
+            grades: 0,
+            borough: 0,
+            restaurant_id: 0,
+        })
+        .limit(10)
+        .toArray();
+
+        response.status(200).json(results);
+    } catch(e) {
+        console.error(e);
+        response.statis(500).json(e);
+    }
+}
